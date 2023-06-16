7f3051eda0beaf6f1a739912ef86a70d30ee20bb
Nazma1997 <nazma.ist.707@gmail.com>
2023-06-16 19:00:42 +0600

    first commit

---
+++ typescript-configuration/output/script.js
@@ -0,0 +1,7 @@
+"use strict";
+const bangladesh = 'I love Bangladesh';
+console.log(bangladesh);
+const inter = 'I read in inter';
+console.log(inter);
+const level = 'I am level';
+console.log(level);
---
+++ typescript-configuration/src/script.ts
@@ -0,0 +1,8 @@
+const bangladesh = 'I love Bangladesh';
+console.log(bangladesh)
+
+const inter = 'I read in inter';
+console.log(inter)
+
+const level = 'I am level';
+console.log(level)
